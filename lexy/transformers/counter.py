e607481e8479ae052b512350cb5b51cf5018afad
Reza Shabani <4766540+shabani1@users.noreply.github.com>
2023-10-21 18:39:55 -0700

    added lexy_transformer decorator (#14)
    # What
    
    This PR adds the `lexy_transformer` decorator. The decorator can be
    imported and applied as follows.
    
    ```python
    from lexy.transformers import lexy_transformer
    
    @lexy_transformer(name="text.embeddings.minilm")
    def text_embeddings(sentences: list[str]) -> torch.Tensor:
        ...
    ```
    
    When applied to a function, the decorator will:
    * Register the function as a celery shared task with the name
    `lexy.transformers.{name}`
    * Add the kwarg `lexy_index_fields` to the function signature
    * When run without the argument, the decorated function behaves as it
    normally would
    * With the argument, the decorated function returns its output with the
    labels specified in `lexy_index_fields` (see example below)
    
    ```python
    @lexy_transformer(name="add_and_subtract")
    def add_and_subtract(a, b):
        return a + b, a - b
    
    add_and_subtract(5, 3)
    # returns (8, 2)
    
    add_and_subtract(5, 3, lexy_index_fields=["sum", "difference"])
    # returns [{'sum': 8, 'difference': 2}]
    ```
    
    # Test plan
    
    Similar to the test plan for #12, though bindings need the additional
    keyword argument `lexy_index_fields`.
    
    - Create a new transformer with the following payload:
    ```json
    {
      "transformer_id": "text.counter.word_counter",
      "path": "lexy.transformers.counter.word_counter",
      "description": "Returns count of words and the longest word"
    }
    ```
    - Create a new index with the following payload:
    ```json
    {
      "index_id": "word_counts",
      "description": "Word counts",
      "index_table_schema": {},
      "index_fields": {
          "word_count": {"type": "int"}, 
          "longest_word": {"type": "string", "optional": true}
      }
    }
    ```
    - Ensure that the index table `zzidx__word_counts` has been created
    - This is a manual step right now, run
    `lexy.core.events.create_new_index_table('word_counts')`
    - Create a new binding with the following payload:
    ```json
    {
      "collection_id": "default",
      "transformer_id": "text.counter.word_counter",
      "index_id": "word_counts",
      "description": "New binding for word counts",
      "execution_params": {},
      "transformer_params": {
        "lexy_index_fields": ["word_count", "longest_word"]
      },
      "filters": {}
    }
    ```
    
    The test should produce tasks for each document in the default
    collection using the `word_counter` transformer, and save the results in
    the index table `zzidx__word_counts`.
    
    - Finally, the existing binding for `default_text_embeddings` (i.e.,
    binding with `id=1`) should be patched with the following payload:
    ```json
    {
      "transformer_params": {
        "lexy_index_fields": ["embedding"]
      }
    }
    ```
    
    Now, any new documents added to the default collection should trigger
    two jobs, one for each binding.

--- lexy/transformers/counter.py
+++ lexy/transformers/counter.py
@@ -1,9 +1,7 @@
-from typing import Any
-
-from celery import shared_task
+from lexy.transformers import lexy_transformer
 
 
-@shared_task(name="lexy.transformers.counter.count_words")
+@lexy_transformer(name="counter.count_words")
 def count_words(text: str) -> dict[str, int]:
     """ Count the number of words in a text.
 
@@ -16,7 +14,6 @@ def count_words(text: str) -> dict[str, int]:
     Examples:
         >>> count_words("This is a test")
         {"This": 1, "is": 1, "a": 1, "test": 1}
-
     """
     counts = {}
     words = text.split(' ')
@@ -28,11 +25,10 @@ def count_words(text: str) -> dict[str, int]:
     return counts
 
 
-@shared_task(name="lexy.transformers.counter.word_counter")
-def word_counter(text: str) -> list[dict[str, Any]]:
+@lexy_transformer(name="counter.word_counter")
+def word_counter(text: str) -> tuple[int, str]:
     """ Testing a transformer. """
     words = text.split()
     word_count = len(words)
     longest_word = max(words, key=len)
-    return [{"word_count": word_count, "longest_word": longest_word}]
-
+    return word_count, longest_word
